ad899dec8efb243226a622910ec1e95b4e263567
Facu1172 <Facu172@hotmail.com>
2022-09-14 00:12:16 -0300

    proyecto integrador

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 //CARGA DE FICHAS DE PERSONAS
-let miPersona
+let Personas= []
+
 alert("Bienvenido al sistema de carga de perfiles, ud podra cargar los datos de una persona y modificarlos")
 
 class persona{
@@ -10,6 +11,9 @@ class persona{
     this.numeracionCalle=numeracionCalle
 }
 modificarNombre = (nuevonombre) => (this.nombre = nuevonombre)
+modificarEdad = (nuevaedad) => (this.edad = nuevaedad)
+modificarCalle = (nuevacalle) => (this.calle = nuevacalle)
+modificarNumeracion = (nuevanumeracion) => (this.numeracionCalle = nuevanumeracion)
 }
 function DatosPersona (){
     let nombre = prompt("ingrese el nombre de la persona a registrar")
@@ -20,17 +24,65 @@ function DatosPersona (){
     let OBJETO_PERSONA = new  persona(nombre,edad,calle,numeracionCalle)
     return OBJETO_PERSONA
 }
-miPersona= DatosPersona()
-alert("Ud ha ingresado a " +miPersona.nombre + " de "+miPersona.edad +" años " + " ,vive en la calle "+ miPersona.calle + " al " + miPersona.numeracionCalle)
-
+function mostrarPersonas() {
+    for (let index = 0; index < Personas.length; index++) {
+        alert("Nombre: " + Personas[index].nombre + " Edad: " + Personas[index].edad + " Calle: " + Personas[index].calle + " " + Personas[index].numeracionCalle);
+    }
+}
 function menu (){
-    OPCION = parseInt(prompt("1- Modificar Nombre \n 2- Modificar edad \n 3- modificar calle \n 4- modificar numeración de calle \n 5- Imprimir datos" ))
+    OPCION = parseInt(prompt("1- Agregar persona \n 2- Modificar nombre \n 3- modificar calle \n 4- modificar edad \n 5-modificar numeracion de calle  \n 6- Imprimir datos" ))
     return OPCION
 }
 
 function opcionMenu (){
     let seleccion = menu()
-    if (seleccion=1){persona.modificarNombre(nuevonombre = prompt("ingrese el nuevo nombre"))
-return persona.modificarNombre}
+    if (seleccion==1){Personas.push(DatosPersona())
+        opcionMenu() }
+if (seleccion==2){
+    let mensajeAMostrar = ""
+    
+    Personas.forEach((persona,index)=>{
+    mensajeAMostrar += index + "- " + persona.nombre + "\n"
+    })
+    
+    let numeroPersona = parseInt(prompt(mensajeAMostrar + "\n Ingrese el numero de la persona a modificar"))
+    Personas[numeroPersona].modificarNombre(nuevonombre = prompt("ingrese el nuevo nombre"))
 
+    opcionMenu()}
+if(seleccion==3) {
+    let mensajeAMostrar = ""
+    
+    Personas.forEach((persona,index)=>{
+    mensajeAMostrar += index + "- " + persona.calle + "\n"
+})
+    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese el numero de la calle a modificar"))
+Personas[numeroPersona].modificarCalle(nuevacalle = prompt("ingrese la nueva calle"))
+
+opcionMenu()
 }
+if(seleccion==4) {
+    let mensajeAMostrar = ""
+    
+    Personas.forEach((persona,index)=>{
+    mensajeAMostrar += index + "- " + persona.edad + "\n"
+})
+    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese la edad a modificar"))
+Personas[numeroPersona].modificarEdad(nuevaedad = prompt("ingrese la nueva edad"))
+
+opcionMenu()
+}
+if(seleccion==5) {
+    let mensajeAMostrar = ""
+    
+    Personas.forEach((persona,index)=>{
+    mensajeAMostrar += index + "- " + persona.numeracionCalle + "\n"
+})
+    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese la numeracion a modificar"))
+Personas[numeroPersona].modificarNumeracion(nuevanumeracion = prompt("ingrese la nueva numeracion"))
+
+opcionMenu()
+}
+if(seleccion==6){(mostrarPersonas())
+    opcionMenu()}
+ }
+opcionMenu()
