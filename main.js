71277fb7e2132ac499c162f99abf30bcc5891f97
Facu1172 <Facu172@hotmail.com>
2022-09-19 22:24:11 -0300

    Carga de fichas completa

--- main.js
+++ main.js
@@ -1,14 +1,15 @@
 //CARGA DE FICHAS DE PERSONAS
 let Personas= []
 
-alert("Bienvenido al sistema de carga de perfiles, ud podra cargar los datos de una persona y modificarlos")
 
 class persona{
-    constructor(nombre,edad,calle,numeracionCalle){
+    id
+constructor(nombre,edad,calle,numeracionCalle){
     this.nombre=nombre
     this.edad=edad
     this.calle=calle
     this.numeracionCalle=numeracionCalle
+    
 }
 modificarNombre = (nuevonombre) => (this.nombre = nuevonombre)
 modificarEdad = (nuevaedad) => (this.edad = nuevaedad)
@@ -16,73 +17,83 @@ modificarCalle = (nuevacalle) => (this.calle = nuevacalle)
 modificarNumeracion = (nuevanumeracion) => (this.numeracionCalle = nuevanumeracion)
 }
 function DatosPersona (){
-    let nombre = prompt("ingrese el nombre de la persona a registrar")
-    let edad = parseInt(prompt("ingrese la edad de la persona"))
-    let calle = prompt("ingrese el nombre de la calle")
-    let numeracionCalle = parseInt(prompt("ingrese el numero de la calle"))
+    let nombre = document.getElementById("nombre").value
+    let edad = document.getElementById("edad").value
+    let calle = document.getElementById("calle").value
+    let numeracionCalle = document.getElementById("numeracion").value
 
     let OBJETO_PERSONA = new  persona(nombre,edad,calle,numeracionCalle)
+
+    if (typeof parseInt(OBJETO_PERSONA.edad)==NaN){alert("Edad incorrecta")
+return null;}
+
     return OBJETO_PERSONA
 }
-function mostrarPersonas() {
-    for (let index = 0; index < Personas.length; index++) {
-        alert("Nombre: " + Personas[index].nombre + " Edad: " + Personas[index].edad + " Calle: " + Personas[index].calle + " " + Personas[index].numeracionCalle);
+
+document.getElementById("formulario").onsubmit = (event) => validarFormulario(event);
+function validarFormulario(event) {
+    event.preventDefault();
+    OBJETO_PERSONA = DatosPersona()
+    if(OBJETO_PERSONA == null){
+        return;
     }
-}
-function menu (){
-    OPCION = parseInt(prompt("1- Agregar persona \n 2- Modificar nombre \n 3- modificar calle \n 4- modificar edad \n 5-modificar numeracion de calle  \n 6- Imprimir datos" ))
-    return OPCION
-}
+OBJETO_PERSONA.id = Personas.length +1    
+Personas.push(OBJETO_PERSONA) 
+AgregarPersonas()}
 
-function opcionMenu (){
-    let seleccion = menu()
-    if (seleccion==1){Personas.push(DatosPersona())
-        opcionMenu() }
-if (seleccion==2){
-    let mensajeAMostrar = ""
-    
-    Personas.forEach((persona,index)=>{
-    mensajeAMostrar += index + "- " + persona.nombre + "\n"
-    })
-    
-    let numeroPersona = parseInt(prompt(mensajeAMostrar + "\n Ingrese el numero de la persona a modificar"))
-    Personas[numeroPersona].modificarNombre(nuevonombre = prompt("ingrese el nuevo nombre"))
 
-    opcionMenu()}
-if(seleccion==3) {
-    let mensajeAMostrar = ""
-    
-    Personas.forEach((persona,index)=>{
-    mensajeAMostrar += index + "- " + persona.calle + "\n"
-})
-    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese el numero de la calle a modificar"))
-Personas[numeroPersona].modificarCalle(nuevacalle = prompt("ingrese la nueva calle"))
+function AgregarPersonas() {
+    let ListaPersonas = document.getElementById("ListaPersonas");
+    ListaPersonas.innerHTML = "";
+    Personas.forEach((persona) => {
+      let column = document.createElement("div");
+      column.className = "col-md-4 mt-3";
+      column.id = `columna-${persona.id}`;
+      column.innerHTML = `
+              <div class="card">
+                  <div class="card-body">
+                  <p class="card-text">ID:
+                      <b>${persona.id}</b>
+                  </p>
+                  <p class="card-text">Nombre:
+                      <b>
+                      <div id= "nombrenuevo-${persona.id}">
+                      ${persona.nombre}
+                      </div>
+                      </b>
+                  </p>
+                  <p class="card-text">Precio edad:
+                      <b>${persona.edad}</b>
+                  </p>
+                  <p class="card-text">Precio calle:
+                      <b>${persona.calle}</b>
+                  </p>
+                  <p class="card-text">Numeracion:
+                      <b>${persona.numeracionCalle}</b>
+                  </p>
+                  </div>
+                  <div class="card-footer">
+                      <button class="btn btn-danger" id="botonEliminar-${persona.id}" >Eliminar</button>
+                  </div>
+                  <div class="card-footer">
+                  <button class="btn btn-caution" id="botonmodificarNombre-${persona.id}" >modificar nombre</button>
+              </div>
+              </div>`;
+              ListaPersonas.append(column);
+              let botonEliminar = document.getElementById(`botonEliminar-${persona.id}`);
+              botonEliminar.onclick = () => eliminarProducto(persona.id);
 
-opcionMenu()
-}
-if(seleccion==4) {
-    let mensajeAMostrar = ""
-    
-    Personas.forEach((persona,index)=>{
-    mensajeAMostrar += index + "- " + persona.edad + "\n"
-})
-    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese la edad a modificar"))
-Personas[numeroPersona].modificarEdad(nuevaedad = prompt("ingrese la nueva edad"))
+              let botonModificarNombre = document.getElementById(`botonmodificarNombre-${persona.id}`);
+              botonModificarNombre.onclick = () => modificarNombre(persona.id);
+            }
+            )
+             }
+             
+            function modificarNombre (id){
+            let NombreModificado = prompt("Ingrese el nuevo nombre")
+            
+            let indicePersonaAbuscar = Personas.findIndex((persona => persona.id == id));
 
-opcionMenu()
-}
-if(seleccion==5) {
-    let mensajeAMostrar = ""
-    
-    Personas.forEach((persona,index)=>{
-    mensajeAMostrar += index + "- " + persona.numeracionCalle + "\n"
-})
-    let numeroPersona =(prompt(mensajeAMostrar + "\n Ingrese la numeracion a modificar"))
-Personas[numeroPersona].modificarNumeracion(nuevanumeracion = prompt("ingrese la nueva numeracion"))
-
-opcionMenu()
-}
-if(seleccion==6){(mostrarPersonas())
-    opcionMenu()}
- }
-opcionMenu()
+            Personas[indicePersonaAbuscar].nombre = NombreModificado
+            document.getElementById ("nombrenuevo-"+id).innerHTML= NombreModificado
+        }
